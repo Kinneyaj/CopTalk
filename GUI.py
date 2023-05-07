f43183e0735ec275a98f881c768087bd0d5ef63d
kylecayne <35715660+kylecayne@users.noreply.github.com>
2023-05-07 14:18:33 -0700

    Agreement
    Changed channel selection to a recording agreement. 
    Made the button text white for the start button so it is easier to see. 
    pass through self to the sdr when we start the sdr so the sdr can use our GUI specifically. 
    Created posts update function

--- GUI.py
+++ GUI.py
@@ -13,7 +13,7 @@ class GUI:
     self.updates_textBox.grid_forget()
     self.posts_button.grid_forget()
     self.stop_button.grid_forget()
-    self.connect_button.grid_forget()
+    self.confirm_button.grid_forget()
     self.warnings_button.grid_forget()
     self.crimes_button.grid_forget()
     self.emergency_button.grid_forget()
@@ -23,16 +23,18 @@ class GUI:
     self.crimes_button.grid_forget()
     self.warnings_button.grid_forget()
     self.live_button.grid_forget()
+    self.agreement_label.grid_forget()
     
   def channel_selected(self):
     '''
     Once a channel is selected this function will run
     It will allow the start button to be pressed
     '''
-    if(self.dropdown_output.get() == "Channel Select"):
-      self.start_button["state"]="disabled"
-    else:
+    
+    if self.dropdown_output.get() == "Agree":
       self.start_button["state"]="normal"
+    else:
+      self.start_button["state"]="disabled"
   def main_menu(self):
     '''
     Resets to home screen:
@@ -47,9 +49,10 @@ class GUI:
       pass
     self.start_button["state"]="disabled"
     self.updating = False
+    self.agreement_label.grid(row=0,column = 0, columnspan =2)
     self.start_button.grid(row=2,column = 0,columnspan = 2)
-    self.connect_button.grid(row=1,column=0,)
-    self.drop.grid(row=1,column = 1)
+    self.confirm_button.grid(row=1,column=1,)
+    self.drop.grid(row=1,column = 0)
     
   def live(self):
     '''
@@ -78,8 +81,22 @@ class GUI:
     self.count+=1
     self.incoming.append(post)
     print(post)
+    self.updates_textBox["state"] = "normal"
     self.updates_textBox.insert(tk.END,post)    
     self.updates_textBox.insert(tk.END,"\n\n")
+    self.updates_textBox["state"]="disabled"
+  def post_update(self,post):  
+    '''
+    accepts: string
+    Will update the screen with the given string in the parameters
+    '''
+    #if(updating):
+    self.outgoing.append(post)
+    print(post)
+    self.posts_textBox["state"] = "normal"
+    self.posts_textBox.insert(tk.END,post)    
+    self.posts_textBox.insert(tk.END,"\n\n")
+    self.posts_textBox["state"]="disabled"
       #root.after(4000,live_update)
   def posts(self):
     '''
@@ -118,25 +135,26 @@ class GUI:
     #Updating Boolean
     updating = False
     #Test Text
-    self.incoming = ["Currently we have a code 4356 at downtown","Fire reported on 5th Avenue"]
-    self.outgoing = ["Recent dangerous crime reported in ohio\nPosted on twitter at 5:30pm","Fire near CSUSM\nPosted on twitter at 7am"]
+    self.incoming = ["Now Recording audio on computer..."]
+    self.outgoing = []
     
     #Label Creation  
     self.copTalk_label = tk.Label(self.root, text = "CopTalk")
+    self.agreement_label = tk.Label(self.root, text = "This application will record the audio from your device")
     #Dropdown options
-    self.channels = ["First","Second"]
+    self.channels = ["Agree","Disagree"]
     
     #DropDown Variable
     self.dropdown_output = tk.StringVar()
-    self.dropdown_output.set("Channel Select")
+    self.dropdown_output.set("Agreement")
     
     #Dropdown Creation
     self.drop = tk.OptionMenu( self.root , self.dropdown_output , *self.channels )
     
     #Button Creation
-    self.start_button = tk.Button(self.root, text = "Start", bg = "green", command = self.live)
-    self.stop_button = tk.Button(self.root, text = "Stop", bg = "red",command = self.main_menu)
-    self.connect_button = tk.Button(self.root, text = "Connect", command = self.channel_selected)
+    self.start_button = tk.Button(self.root, text = "Start", bg = "green", command = self.live, fg = "white")
+    self.stop_button = tk.Button(self.root, text = "Exit", bg = "red",command = self.main_menu)
+    self.confirm_button = tk.Button(self.root, text = "Confirm", command = self.channel_selected)
     self.posts_button = tk.Button(self.root, text = "Posts",bg = "light blue", command = self.posts)
     self.crimes_button = tk.Button(self.root, text ="Crimes",bg = "red")
     self.emergency_button = tk.Button(self.root, text = "Emergency", bg = "yellow")
@@ -144,33 +162,30 @@ class GUI:
     self.live_button = tk.Button(self.root, text = "Live", bg = "light blue", command = self.live)
     #TextBox creation
     self.updates_textBox = tk.Text(self.root)
-    for i in range(len(self.incoming)):
-      self.updates_textBox.insert(tk.END,self.incoming[i])
-      self.updates_textBox.insert(tk.END,"\n\n")
     self.posts_textBox = tk.Text(self.root)
-    for i in range(len(self.outgoing)):
-      self.posts_textBox.insert(tk.END,self.outgoing[i])
-      self.posts_textBox.insert(tk.END,"\n\n")
+    self.updates_textBox.insert(tk.END,self.incoming[0])
+    self.updates_textBox["state"]="disabled"
+    self.posts_textBox["state"]="disabled"
+    st = len(self.incoming[0])
+    st = "-"*st
+    self.updates_textBox.insert(tk.END,("\n"+st+"\n"))
     #Starting Grid Placements
     self.copTalk_label.grid(row= 0,column = 0, columnspan =2)
-    
-    self.updates_textBox.tag_config("red", foreground="red")
-    self.updates_textBox.tag_add("red", "1.7", "1.22")
     self.main_menu() #Places the main menu
-    self.st = TextAnalyzer.rawText("Metro 9417 Alpha 417 Imperial detention 239 contest 865 Imperial Beach Boulevard deserve 53 copy will generate a walk-up response, Crash, accident")
-    self.highlights,self.st = self.colorTostr(self.st)
-    self.row = (len(self.updates_textBox.get("1.0", "end-1c").split("\n")))  
-    self.live_update(self.st)
-    for i in range(len(self.highlights)):
-      start = str(self.row)+"."+str(self.highlights[i][0])
-      end = str(self.row)+"."+str(self.highlights[i][1])
-      self.updates_textBox.tag_add("red", start,end)
+    #self.st = TextAnalyzer.rawText("Metro 9417 Alpha 417 Imperial detention 239 contest 865 Imperial Beach Boulevard deserve 53 copy will generate a walk-up response, Crash, accident")
+    #self.highlights,self.st = self.colorTostr(self.st)
+    #self.row = (len(self.updates_textBox.get("1.0", "end-1c").split("\n")))  
+    #self.live_update(self.st)
+    #for i in range(len(self.highlights)):
+    #  start = str(self.row)+"."+str(self.highlights[i][0])
+    #  end = str(self.row)+"."+str(self.highlights[i][1])
+    #  self.updates_textBox.tag_add("red", start,end)
     #live_update()
     #Starting Button States
     self.start_button["state"]="disabled"
     self.sp = SignalProc.SignalProcessor()
     #sp.sdr_start()
-    self.thread = threading.Thread(target=self.sp.sdr_start)
+    self.thread = threading.Thread(target= lambda : self.sp.sdr_start(self))
     self.thread.start()
   def startGUI(self):
     
