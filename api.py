306ac195078f63089af5bdf84e0f3bf4a2005b22
kylecayne <35715660+kylecayne@users.noreply.github.com>
2023-05-07 21:37:55 -0700

    Update api.py

--- api.py
+++ api.py
@@ -17,15 +17,15 @@ def twitterPost(textTwitter):
   return
 
   
-  def postTweet(passedString,bearer_token,API_key, API_secret,access_token, access_token_secret):
-    textTwitter = passedString
-    
-    client = tweepy.Client(bearer_token,API_key, API_secret,access_token, access_token_secret)
-    
-    auth = tweepy.OAuth1UserHandler(bearer_token,API_key, API_secret,access_token, access_token_secret)
-  
-    response = client.create_tweet(text = textTwitter)
-    print(response)
+def postTweet(passedString,bearer_token,API_key, API_secret,access_token, access_token_secret):
+  textTwitter = passedString
+
+  client = tweepy.Client(bearer_token,API_key, API_secret,access_token, access_token_secret)
+
+  auth = tweepy.OAuth1UserHandler(bearer_token,API_key, API_secret,access_token, access_token_secret)
+
+  response = client.create_tweet(text = textTwitter)
+  print(response)
   
   postTweet(bearer_token,API_key, API_secret,access_token, access_token_secret)
 
